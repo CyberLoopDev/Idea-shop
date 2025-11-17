ee05560fa6e9921f2eea1e2336a98feda17c82bc
CyberLoopdev <bilalisakhanov@gmail.com>
2025-11-17 19:12:26 +0600

    lastv

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,42 @@
-import './Modal.css'
-import { categoriesData } from '../../data/componentsData'
-import { Link } from 'react-router-dom'
+import { useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { CustomContext } from '../../store/store'; // Убедитесь, что путь к контексту правильный
+
+import Filter from "./Filter/Filter";
+import FilteredProducts from "./FilteredProducts/FilteredProducts";
 
 
-const Modal = ({ closeForm }) => {
-  return (
+const Catalog = () => {
+    // 1. Получаем динамический параметр из URL
+    const { categorySlug } = useParams(); 
+    
+    // 2. Получаем функцию установки фильтра из контекста
+    const { setFilter } = useContext(CustomContext); 
+    
+    useEffect(() => {
+        // Этот эффект срабатывает при первом рендере и при смене categorySlug в URL
+        if (categorySlug) {
+            // Если slug существует, устанавливаем фильтр 'category' в контексте
+            setFilter(prev => ({
+                ...prev,
+                category: categorySlug // Например, 'girls', 'boys'
+            }));
+        } else {
+            // Если мы находимся на чистом /catalog, удаляем фильтр категории
+             setFilter(prev => {
+                const { category, ...rest } = prev; 
+                return rest; 
+            });
+        }
+    // Зависимости: effect должен перезапускаться при изменении slug или функции setFilter
+    }, [categorySlug, setFilter]); 
 
- 
-  <div className="showform-overlay">
-    <div className="showform-box">
-      <button className="close-form" onClick={closeForm}>×</button>
+    return (
+        <main style={{ minWidth: '1000px'}}>
+            <Filter />
+            <FilteredProducts />
+        </main>
+    );
+};
 
-      <div className="showform-grid">
-       
-
-        {categoriesData.items.map(item => (
-            <div className="item">
-                <Link to={item.url}><img className='showform-img' src={item.image} alt="" /> </Link> 
-                <p>{item.name}</p>        
-            </div>
-        ))}
-
-
-       </div>
-            
-         
-    </div>
-  </div>
-
-
-
-  )
-}
-
-export default Modal
+export default Catalog;
