8af12643f120a46a5dc091a68b3439eff8f1d3e8
CyberLoopdev <bilalisakhanov@gmail.com>
2025-11-19 19:33:32 +0600

    lastv111211v

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,28 +1,37 @@
-import { Outlet } from 'react-router-dom'
-import './Profile.css'
-import { NavLink } from 'react-router-dom'
-
+import { Outlet, NavLink, useNavigate } from 'react-router-dom';
+import './Profile.css';
 
 const Profile = () => {
+  const navigate = useNavigate();
 
-
+  const handleLogout = () => {
+    
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    
+    
+    navigate('/login');
+  };
 
   return (
     <div className='profile'>
-       <aside className='navbar'>
+      <aside className='navbar'>
         <ul className='list-links'>
-          <li className='link'><NavLink to='orders-user'>История заказов</NavLink></li>
-          <li className='link'><NavLink to='contacts-data'>Контактные данные</NavLink></li>
-          <li className='link'>Выход</li>
-          </ul>
-          </aside> 
-          
+          <li className='link'>
+            <NavLink to='orders-user'>История заказов</NavLink>
+          </li>
+          <li className='link'>
+            <NavLink to='contacts-data'>Контактные данные</NavLink>
+          </li>
+          <li className='link' onClick={handleLogout}>Выход</li>
+        </ul>
+      </aside> 
 
-          <main className='profile-lyt'>
-            <Outlet /></main>      
-
+      <main className='profile-lyt'>
+        <Outlet />
+      </main>      
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
